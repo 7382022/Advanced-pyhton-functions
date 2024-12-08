87fb3b6363887cd43e9b78f01bb279676af9796f
7382022 <amreenrida2@gmail.com>
2024-12-08 10:28:37 -0500

    exit

---
+++ using exit.py
@@ -0,0 +1,14 @@
+# python progran to demonstrate
+#exit(
+
+for i in range(10):
+    #If the value of i becomes
+    # 5 then the program id forced
+    # to exit
+    if i == 5:
+
+
+        # prints the exits message
+        print(exit)
+        exit()
+    print(i)
